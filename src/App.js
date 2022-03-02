4fdc086c9c4f4ac475295650f03a8cfbf8b75b48
Manuel Pavon <manuelpr11@msn.com>
2022-03-02 10:22:22 -0600

    add addition component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Addition from './components/addition/Addition';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Demo Calc
         </p>
+	<Addition />
         <a
           className="App-link"
           href="https://reactjs.org"
